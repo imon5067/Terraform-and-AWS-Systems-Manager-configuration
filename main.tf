6eabcea90ea8b18522e21967a28b868df1592fb3
O R Imon <orimon9@gmail.com>
2024-02-04 17:48:10 +0200

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,75 @@
+data "aws_ami" "amazon-2" {
+  most_recent = true
+
+  filter {
+    name = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-ebs"]
+  }
+  owners = ["amazon"]
+}
+
+variable "vpc_id" {
+    default = "vpc-0fdaa1285dce5c73e"
+}
+
+variable "subnet_id" {
+    default = "subnet-063a137667ef271da"
+}
+
+variable "key_name" {
+    #default = "chavessh"
+    default = "sshkey1"
+}
+
+
+resource "aws_security_group" "permitir_ssh" {
+  name        = "permitir_ssh"
+  description = "Permite SSH na instancia EC2"
+  vpc_id      = var.vpc_id
+
+  ingress {
+    description = "SSH to EC2"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "permitir_ssh"
+  }
+}
+
+resource "aws_instance" "webserver1" {
+  ami           = data.aws_ami.amazon-2.id
+  instance_type = "t2.micro"
+  key_name = var.key_name
+  subnet_id = var.subnet_id
+  vpc_security_group_ids = [aws_security_group.permitir_ssh.id]
+  associate_public_ip_address = true
+
+  tags = {
+    Name = "webserver1"
+  }
+}
+
+
+resource "aws_instance" "webserver2" {
+  ami           = data.aws_ami.amazon-2.id
+  instance_type = "t2.micro"
+  key_name = var.key_name
+  subnet_id = var.subnet_id
+  vpc_security_group_ids = [aws_security_group.permitir_ssh.id]
+  associate_public_ip_address = true
+
+  tags = {
+    Name = "webserver2"
+  }
+}
---
+++ provider.tf
@@ -0,0 +1,5 @@
+provider "aws" {
+    region = "us-east-1"
+    # access_key = "AKIAXN5DO4M4X7D7QUVI"
+    # secret_key = "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"
+}
